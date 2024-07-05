b65f0940716f0bd23231b13b8e25089c9fd050ae
Eli RoCas <elir_94@hotmail.com>
2024-07-05 14:45:42 -0500

    Routes configuration in app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,15 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter, RouterModule} from '@angular/router';
+import { provideRouter, RouterModule } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { importProvidersFrom } from '@angular/core';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(),
+    importProvidersFrom(RouterModule)
+  ],
 };
