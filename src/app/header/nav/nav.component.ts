74197c76237ac5b7c9f5b2f7094572ffcc75585f
Eli RoCas <elir_94@hotmail.com>
2024-07-05 16:39:33 -0500

    nav config

--- src/app/header/nav/nav.component.ts
+++ src/app/header/nav/nav.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.scss'
 })
 export class NavComponent {
+  isOpen = false;
+  isDarkMode = false;
 
+  toggleDarkMode() {
+    this.isDarkMode = !this.isDarkMode;
+    if (this.isDarkMode) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }
 }
