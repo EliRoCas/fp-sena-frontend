c8efc58332445bc7e077f78bddb43e42e82e37bf
Eli RoCas <elir_94@hotmail.com>
2024-07-11 17:47:34 -0500

    Test  templetes

--- src/app/users/user-profile/user-profile.component.ts
+++ src/app/users/user-profile/user-profile.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { UserCardComponent } from '../user-card/user-card.component';
+import { UserFormComponent } from '../user-form/user-form.component';
 
 @Component({
   selector: 'app-user-profile',
   standalone: true,
-  imports: [],
+  imports: [UserCardComponent, UserFormComponent],
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.scss'
 })
