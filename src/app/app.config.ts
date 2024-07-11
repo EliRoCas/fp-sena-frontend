b57348241178a3101aee0f21946978f17e70048a
Eli RoCas <elir_94@hotmail.com>
2024-07-11 13:46:50 -0500

    Angular Material added

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,12 +4,13 @@ import { provideHttpClient } from '@angular/common/http';
 import { importProvidersFrom } from '@angular/core';
 
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(),
-    importProvidersFrom(RouterModule)
+    importProvidersFrom(RouterModule), provideAnimationsAsync()
   ],
 };
