07d2dfa1562d71426cf201ec5fa1d2065713be1a
Eli RoCas <elir_94@hotmail.com>
2024-07-12 11:56:25 -0500

    user-form

--- src/app/users/register-user/register-user.component.ts
+++ src/app/users/register-user/register-user.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { UserFormComponent } from '../../share/user-form/user-form.component';
 
 @Component({
   selector: 'app-register-user',
   standalone: true,
-  imports: [],
+  imports: [UserFormComponent],
   templateUrl: './register-user.component.html',
   styleUrl: './register-user.component.scss'
 })
