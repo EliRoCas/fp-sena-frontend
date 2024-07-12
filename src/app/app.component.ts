c3b075feb298d29d3d1ee48ca9115b30b89e33b0
EliRoCas <elirocas946@gmail.com>
2024-07-11 21:20:29 -0500

    Add and apply  ea-layout package

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { LayoutComponent} from '@control-ea/layout'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterModule, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, RouterModule, HeaderComponent, FooterComponent, LayoutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
