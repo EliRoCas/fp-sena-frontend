f3069000af05a6f4ac6e5d019f1e36396b5becd3
Eli RoCas <elir_94@hotmail.com>
2024-07-25 16:01:48 -0500

    Injectable descomment

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,12 +1,12 @@
-// import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class ProductsService {
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService {
 
-//   constructor() { }
-// }
+  constructor() { }
+}
 
 import { EntityAdapter } from '@ea-controls/ngrx-repository'; // Manejador de operaciones para entidades NGRX
 import { createSelector } from '@ngrx/store'; // Función de la biblioteca NGRX para crar selectores de estado memorizados
--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,3 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+
+  constructor() { }
+}
+
 
 import { EntityAdapter } from '@ea-controls/ngrx-repository'; // Manejador de operaciones para entidades NGRX
 import { createSelector } from '@ngrx/store'; // Función de la biblioteca NGRX para crar selectores de estado memorizados
