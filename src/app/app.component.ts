5a88348e48d97eb9a72289cfa2ae1996936af204
Eli RoCas <elir_94@hotmail.com>
2024-07-25 14:47:31 -0500

    SideNav configured with full app's links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,41 @@
 import { Component } from '@angular/core';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './structure/header/header.component';
 import { FooterComponent } from './structure/footer/footer.component';
 import { LayoutComponent } from '@ea-controls/layout'
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { PortalComponent, PortalContentComponent } from '@ea-controls/portal';
+import { NavComponent } from './structure/header/nav/nav.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterModule, HeaderComponent, FooterComponent, LayoutComponent],
+  imports: [
+    RouterOutlet, 
+    RouterModule, 
+    HeaderComponent, 
+    FooterComponent, 
+    LayoutComponent,
+    CommonModule,
+    RouterLink,
+    PortalComponent,
+    LayoutComponent,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+    PortalComponent,
+    PortalContentComponent,
+    MatListModule,
+    MatDividerModule,
+    RouterModule,
+    RouterOutlet,
+    NavComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/structure/header/nav/nav.component.ts
+++ src/app/structure/header/nav/nav.component.ts
@@ -1,30 +1,52 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, input, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
-import { PortalComponent} from '@ea-controls/portal';
+import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
+import { PortalComponent, PortalContentComponent } from '@ea-controls/portal';
+import { LayoutComponent } from '@ea-controls/layout';
+import { MatDrawer, MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [CommonModule, RouterLink, PortalComponent],
+  imports: [
+    CommonModule,
+    RouterLink,
+    PortalComponent,
+    LayoutComponent,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+    PortalComponent,
+    PortalContentComponent,
+    MatListModule,
+    MatDividerModule,
+    RouterModule,
+    RouterOutlet
+  ],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.scss',
   encapsulation: ViewEncapsulation.None
 })
 export class NavComponent {
-  isOpen = false;
-  isDarkMode = false;
 
-  toggleDarkMode() {
-    this.isDarkMode = !this.isDarkMode;
-    if (this.isDarkMode) {
-      document.documentElement.classList.add('dark');
-    } else {
-      document.documentElement.classList.remove('dark');
-    }
-  }
+  drawer = input.required<MatDrawer>()
+  // isOpen = false;
+  // isDarkMode = false;
 
-  toggleMenu() {
+  // toggleDarkMode() {
+  //   this.isDarkMode = !this.isDarkMode;
+  //   if (this.isDarkMode) {
+  //     document.documentElement.classList.add('dark');
+  //   } else {
+  //     document.documentElement.classList.remove('dark');
+  //   }
+  // }
 
-  }
+  // toggleMenu() {
+
+  // }
 }
