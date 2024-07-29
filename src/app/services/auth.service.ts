a6772c0bad087337b6d837cfa87694c0d81d06b2
Eli RoCas <elir_94@hotmail.com>
2024-07-29 16:00:56 -0500

    Login Auth  with guard, interceptor and JWT (schema)

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { CookieService } from 'ngx-cookie-service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private loginUrl = `http://localhost/sigef-final-proyect/Backend/controller/`;
+  constructor(private http: HttpClient, private cookieService: CookieService) { }
+
+  login(credentials: { email: string, password: string }): Observable<any> {
+    return this.http.post<any>(this.loginUrl, credentials).pipe(
+      tap(response => {
+        this.cookieService.set('token', response.token);
+      })
+    );
+  }
+
+  logout(): void {
+    this.cookieService.delete('token');
+  }
+
+  isLoggedIn(): boolean {
+    return this.cookieService.check('token');
+  }
+}
