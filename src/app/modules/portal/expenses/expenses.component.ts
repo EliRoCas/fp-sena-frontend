508fdf9edca3aba69f6a8fe0be336b30d9624725
Eli RoCas <elir_94@hotmail.com>
2024-07-31 17:07:05 -0500

    mobile view expenses and incomes

--- src/app/modules/portal/expenses/expenses.component.ts
+++ src/app/modules/portal/expenses/expenses.component.ts
@@ -1,7 +1,7 @@
-import { Component, signal} from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { RouterLink } from '@angular/router';
-import { DatePipe } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIcon } from '@angular/material/icon';
@@ -9,6 +9,9 @@ import { MatInputModule } from '@angular/material/input';
 import { PortalContentComponent } from '@ea-controls/portal';
 import { transactionAdapter, transactionExpense, TransactionModel } from '../../../services/transactions.service';
 import { Store } from '@ngrx/store';
+import { BreakpointObserver, Breakpoints, LayoutModule } from '@angular/cdk/layout';
+import { Subject, takeUntil } from 'rxjs';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @Component({
   selector: 'app-expenses',
@@ -22,11 +25,17 @@ import { Store } from '@ngrx/store';
     MatButtonModule,
     MatIcon,
     PortalContentComponent,
+    MatExpansionModule,
+    CommonModule,
+    LayoutModule
   ],
   templateUrl: './expenses.component.html',
   styleUrl: './expenses.component.scss',
 })
 export class ExpensesComponent {
+  readonly panelOpenState = signal(false);
+  isSmallScreen = false;
+
   displayedColumns: string[] = [
     'id_transaction',
     'transaction_name',
@@ -38,16 +47,30 @@ export class ExpensesComponent {
   dataSource = new MatTableDataSource<TransactionModel>()
   selected = signal<TransactionModel | undefined>(undefined);
 
+  private destroy$ = new Subject<void>();
+
   constructor(
-    private store: Store
+    private store: Store,
+    private breakpointObserver: BreakpointObserver
   ) { }
 
   ngOnInit(): void {
     this.store.dispatch(transactionAdapter.getAll());
     this.store.select(transactionExpense).subscribe(data => this.dataSource.data = data);
 
+    const customBreakpoint = '(max-width: 800px)';
+    this.breakpointObserver
+      .observe([customBreakpoint])
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(result => {
+        this.isSmallScreen = result.matches;
+      });
   }
 
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
   delete(transaction: TransactionModel) {
     this.store.dispatch(transactionAdapter.removeOne(transaction));
   }
--- src/app/modules/portal/incomes/incomes.component.ts
+++ src/app/modules/portal/incomes/incomes.component.ts
@@ -1,14 +1,17 @@
-import { DatePipe } from '@angular/common';
-import { Component, signal } from '@angular/core';
+import { CommonModule, DatePipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIcon } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatExpansionModule } from '@angular/material/expansion';
 import { RouterLink } from '@angular/router';
 import { PortalContentComponent } from '@ea-controls/portal';
 import { transactionAdapter, transactionIncome, TransactionModel } from '../../../services/transactions.service';
 import { Store } from '@ngrx/store';
+import { BreakpointObserver, Breakpoints, LayoutModule } from '@angular/cdk/layout';
+import { Subject, takeUntil } from 'rxjs';
 
 
 @Component({
@@ -23,11 +26,17 @@ import { Store } from '@ngrx/store';
     MatButtonModule,
     MatIcon,
     PortalContentComponent,
+    MatExpansionModule,
+    CommonModule,
+    LayoutModule
   ],
   templateUrl: './incomes.component.html',
   styleUrl: './incomes.component.scss'
 })
 export class IncomesComponent {
+  readonly panelOpenState = signal(false);
+  isSmallScreen = false;
+
   displayedColumns: string[] = [
     'id_transaction',
     'transaction_name',
@@ -37,18 +46,35 @@ export class IncomesComponent {
     'transaction_rose_export',
     'transaction_customer',
     'incomeActions'];
+
   dataSource = new MatTableDataSource<TransactionModel>();
   selected = signal<TransactionModel | undefined>(undefined);
+  
+  private destroy$ = new Subject<void>();
 
   constructor(
-    private store: Store
+    private store: Store,
+    private breakpointObserver: BreakpointObserver
   ) { }
 
   ngOnInit(): void {
     this.store.dispatch(transactionAdapter.getAll());
-    this.store.select(transactionIncome).subscribe(data => this.dataSource.data = data);
+    this.store.select(transactionIncome).subscribe(data => {
+      this.dataSource.data = data
+    });
 
-    //this.store.select(transactionIncome).subscribe;
+    const customBreakpoint = '(max-width: 800px)';
+    this.breakpointObserver
+      .observe([customBreakpoint])
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(result => {
+        this.isSmallScreen = result.matches;
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 
   delete(transaction: TransactionModel) {
