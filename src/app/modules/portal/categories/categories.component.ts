9ff709ad81bdd304faadb652931bb5631ff589dc
Eli RoCas <elir_94@hotmail.com>
2024-08-30 16:43:02 -0500

    fixed: mobile category table

--- src/app/modules/portal/categories/categories.component.ts
+++ src/app/modules/portal/categories/categories.component.ts
@@ -11,6 +11,10 @@ import { MatDialog } from '@angular/material/dialog';
 import { CategoryFormComponent } from './category-form/category-form.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { environment } from '../../../../../environment';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { Subject, takeUntil } from 'rxjs';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @Component({
   selector: 'app-categories',
@@ -23,12 +27,15 @@ import { MatInputModule } from '@angular/material/input';
     FilterComponent,
     ReactiveFormsModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatExpansionModule
   ],
   templateUrl: './categories.component.html',
   styleUrl: './categories.component.scss'
 })
 export class CategoriesComponent {
+  readonly panelOpenState = signal(false);
+  isSmallScreen = false;
 
   displayedColumns: string[] = ['id_category', 'category_name', 'catActions'];
   dataSource = new MatTableDataSource<CategoryModel>();
@@ -36,17 +43,31 @@ export class CategoriesComponent {
   id = input<number | undefined>();
   filterValue = "";
 
-
+  private destroy$ = new Subject<void>();
 
   constructor(
     private store: Store,
-    private matDialog: MatDialog
+    private matDialog: MatDialog, 
+    private breakpointObserver: BreakpointObserver
   ) { }
 
   ngOnInit(): void {
     this.store.dispatch(catAdapter.getAll());
     this.store.select(catAdapter.feature).subscribe(data => this.dataSource.data = data);
+
+    const customBreakpoint = '(max-width: 800px)';
+    this.breakpointObserver
+      .observe([customBreakpoint])
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(result => {
+        this.isSmallScreen = result.matches;
+      });
   };
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 
   delete(category: CategoryModel) {
     this.store.dispatch(catAdapter.removeOne(category))
@@ -66,4 +87,12 @@ export class CategoriesComponent {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
+
+  apk(): boolean {
+    if (environment.isMobile){
+      return true; 
+    }else {
+      return false;
+    }
+  };
 }
