e5041d3273b22be0100c4b557d096a7bf11e8587
Eli RoCas <elir_94@hotmail.com>
2024-08-30 18:16:05 -0500

    add: form support service simulation.

--- src/app/structure/dashboard/support/support.component.ts
+++ src/app/structure/dashboard/support/support.component.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-support',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './support.component.html',
-  styleUrl: './support.component.scss'
+  styleUrl: './support.component.scss',
 })
 export class SupportComponent {
+  supportForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.supportForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required],
+    });
+  }
+
+  // Se guardan el "mensaje" en un archivo de texto de forma local. 
+  onSubmit() {
+    if (this.supportForm.valid) {
+      const file = new Blob([JSON.stringify(this.supportForm.value)], {
+        type: 'text/plain',
+      });
+      const anchor = document.createElement('a');
+      anchor.href = URL.createObjectURL(file);
+      anchor.download = 'supportFormData.txt';
+      anchor.click();
+      URL.revokeObjectURL(anchor.href);
+    }
+  }
 }
